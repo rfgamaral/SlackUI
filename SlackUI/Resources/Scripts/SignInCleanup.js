736d7e55a5279b0071318a47e1815081f47292e3
Ricardo Amaral <master@ricardoamaral.net>
2014-11-16 15:21:44 +0000

    Improve CSS elements clean up script

--- SlackUI/Resources/Scripts/SignInCleanup.js
+++ SlackUI/Resources/Scripts/SignInCleanup.js
@@ -3,9 +3,11 @@
  */
 (function (d) {
 
-    d.querySelector(".header_nav").style.display = 'none';
+    d.querySelector("header .header_nav").style.display = 'none';
 
-    d.querySelector("#page_contents > .align_center:last-child").style.display = 'none';
+    d.querySelector("#page").style.minHeight = 0;
+
     d.querySelector("#page_contents").style.paddingBottom = 0;
+    d.querySelector("#page_contents > .align_center:last-child").style.display = 'none';
 
 }(document));
