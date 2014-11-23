569cf0dce738ad6521ffa04e1e5288bb7c604906
Ricardo Amaral <master@ricardoamaral.net>
2014-11-23 14:41:22 +0000

    Use 'Url' instead of 'Address' for private fields

--- SlackUI/Core/Program.cs
+++ SlackUI/Core/Program.cs
@@ -22,7 +22,7 @@ namespace SlackUI {
         private const string CacheFolderName = "Cache";
         private const string LogFileName = "Chromium.log";
         private const string SettingsFileName = "settings.xml";
-        private const string SlackTeamAddress = "https://{0}.slack.com";
+        private const string SlackTeamUrl = "https://{0}.slack.com";
 
         #endregion
 
@@ -32,7 +32,7 @@ namespace SlackUI {
          * Active team domain address.
          */
         internal static string ActiveTeamAddress {
-            get { return String.Format(SlackTeamAddress, Settings.Data.InitialTeamToLoad); }
+            get { return String.Format(SlackTeamUrl, Settings.Data.InitialTeamToLoad); }
         }
 
         /*
