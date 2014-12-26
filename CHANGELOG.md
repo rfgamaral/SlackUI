0b20c55362f4566b7d1d46fb123fc4e6f3198bba
Ricardo Amaral <master@ricardoamaral.net>
2014-12-26 14:22:19 +0000

    Update version attributes and changelog for new public release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+# Version 1.0.14360.1435
+
+#### 2014/12/26
+
+ - Upgrade CefSharp to stable v37.0.0 based on Chromium 37.
+ - Open all external links with default system browser.
+ - Prevent pressing the backspace key to browse back.
+ - Separate cookies and cache storage locations.
+ - Minor bug fixes and improvements.
+
 # Version 1.0.14306.1755
 
 #### 2014/11/02
--- SlackUI/Properties/AssemblyInfo.cs
+++ SlackUI/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("5f2985ca-798c-4efd-a3c8-031846960a45")]
 
-[assembly: AssemblyVersion("1.0.14306.1755")]
-[assembly: AssemblyFileVersion("1.0.14306.1755")]
+[assembly: AssemblyVersion("1.0.14360.1435")]
+[assembly: AssemblyFileVersion("1.0.14360.1435")]
 
 [assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: CLSCompliant(true)]
