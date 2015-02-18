8781a9e59d38fd312eaa23c52a11d27ec32910c0
Michael Miller <Vension@gmail.com>
2015-02-17 21:29:40 -0600

    Not finished.

---
+++ SlackUI/Core/ChromiumNotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackUI.Core {
+    class ChromiumNotification {
+        public void ShowNotification(String title, String body, String icon, String tag) {
+            System.Diagnostics.Debug.WriteLine("[Notification] title: '{0}', body: '{1}', icon: '{2}', tag: '{3}'.", title, body, icon, tag);
+        }
+    }
+}
