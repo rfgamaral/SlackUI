58ea9dc6b26c804f06f852a0f9d8f95997fca552
Ricardo Amaral <master@ricardoamaral.net>
2015-02-21 22:53:06 +0000

    Small redesign to browser load overlay

--- SlackUI/Forms/WrapperForm.Designer.cs
+++ SlackUI/Forms/WrapperForm.Designer.cs
@@ -26,6 +26,7 @@
             this.browserPanel = new System.Windows.Forms.Panel();
             this.browserLoadOverlay = new System.Windows.Forms.Panel();
             this.loadingMarquee = new System.Windows.Forms.ProgressBar();
+            this.connectingLabel = new System.Windows.Forms.Label();
             this.browserPanel.SuspendLayout();
             this.browserLoadOverlay.SuspendLayout();
             this.SuspendLayout();
@@ -43,8 +44,8 @@
             // browserLoadOverlay
             // 
             this.browserLoadOverlay.BackgroundImage = global::SlackUI.Properties.Resources.PanelBackground;
-            this.browserLoadOverlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.browserLoadOverlay.Controls.Add(this.loadingMarquee);
+            this.browserLoadOverlay.Controls.Add(this.connectingLabel);
             this.browserLoadOverlay.Dock = System.Windows.Forms.DockStyle.Fill;
             this.browserLoadOverlay.Location = new System.Drawing.Point(0, 0);
             this.browserLoadOverlay.Name = "browserLoadOverlay";
@@ -53,14 +54,27 @@
             // 
             // loadingMarquee
             // 
-            this.loadingMarquee.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.loadingMarquee.Location = new System.Drawing.Point(12, 569);
+            this.loadingMarquee.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.loadingMarquee.Location = new System.Drawing.Point(152, 291);
             this.loadingMarquee.MarqueeAnimationSpeed = 5;
             this.loadingMarquee.Name = "loadingMarquee";
-            this.loadingMarquee.Size = new System.Drawing.Size(920, 15);
+            this.loadingMarquee.Size = new System.Drawing.Size(640, 15);
             this.loadingMarquee.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
             this.loadingMarquee.TabIndex = 0;
+            // 
+            // connectingLabel
+            // 
+            this.connectingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectingLabel.BackColor = System.Drawing.Color.Transparent;
+            this.connectingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectingLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.connectingLabel.Location = new System.Drawing.Point(152, 243);
+            this.connectingLabel.Margin = new System.Windows.Forms.Padding(5);
+            this.connectingLabel.Name = "connectingLabel";
+            this.connectingLabel.Size = new System.Drawing.Size(640, 40);
+            this.connectingLabel.TabIndex = 1;
+            this.connectingLabel.Text = "Connecting to team site…";
+            this.connectingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // WrapperForm
             // 
@@ -85,6 +99,7 @@
         private System.Windows.Forms.Panel browserPanel;
         private System.Windows.Forms.Panel browserLoadOverlay;
         private System.Windows.Forms.ProgressBar loadingMarquee;
+        private System.Windows.Forms.Label connectingLabel;
 
     }
 }
