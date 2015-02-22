a99bafdddd85db93a5b2d1d837ae6433fd539f0d
Michael Miller <Vension@gmail.com>
2015-02-22 12:07:57 -0600

    Implement notification using Toaster.

--- SlackUI/Core/ChromiumNotification.cs
+++ SlackUI/Core/ChromiumNotification.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SlackUI.Core {
     class ChromiumNotification {
+
         public void ShowNotification(String title, String body, String icon, String tag) {
+            Program.WrapperForm.InvokeOnUiThreadIfRequired(() => {
+                Toaster.Toast.ShowNotification(title, body, icon, tag);
+            });
+
             System.Diagnostics.Debug.WriteLine("[Notification] title: '{0}', body: '{1}', icon: '{2}', tag: '{3}'.", title, body, icon, tag);
         }
     }
